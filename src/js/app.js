672c1dbed0eea1bb3b6f21689335b1142255046c
Carlo Autor <carloautor@gmail.com>
2023-03-10 12:38:07 +0800

    Added style, get new button
    Added style for modern looking page
    
    Added 'Get new' button for the user to not refresh the page
    
    Added a function that if the author is null, replace with 'Unknown Author'
    
    Working on:
    Copy button
    Layout for bigger screens or viewport

--- src/js/app.js
+++ src/js/app.js
@@ -1,42 +1,83 @@
+const quoteParagraph = document.querySelector('#quote');
+const authorParagraph = document.querySelector('#author');
+const quoteID = document.querySelector('#quoteID');
+const quoteCount = document.querySelector('#quoteCount');
+
+const newQuoteBtn = document.querySelector('#newQuoteBtn');
+const copyBtn = document.querySelector('#copyBtn');
+
 const getApi = async () => {
 	try {
 		const apiUrl = `https://type.fit/api/quotes`;
 		const fetchApi = await fetch(apiUrl);
 		const parsedData = await fetchApi.json();
 		return parsedData;
+		//array of objects ang nilalabas neto based sa
+		// structure ng API itself
 	} catch (e) {
 		console.log(e);
 	}
 };
 
 const getRandomQuote = (data) => {
-	//Dapat pag null ang author lagyan natin ng placeholder
 	return new Promise((resolve, reject) => {
 		if (data) {
 			let randomIndex = Math.floor(Math.random() * data.length);
 			const author = data[randomIndex].author;
 			const quote = data[randomIndex].text;
-			resolve({
-				quote: quote,
-				author: author,
-				number: randomIndex,
-				quotesCount: data.length,
-			});
+
+			if (author === null) {
+				resolve({
+					quote: quote,
+					author: `Unknown Author`,
+					number: randomIndex,
+					quotesCount: data.length,
+				});
+			} else {
+				resolve({
+					quote: quote,
+					author: author,
+					number: randomIndex,
+					quotesCount: data.length,
+				});
+			}
 		} else {
 			reject('Cannot fetch data');
 		}
 	});
 };
 
-const appendInformation = (data) => {
-	const quoteParagraph = document.querySelector('#quote');
-	const authorParagraph = document.querySelector('#author');
-	const quoteID = document.querySelector('#quoteID');
-	const quoteCount = document.querySelector('#quoteCount');
-	quoteParagraph.textContent = data.quote;
-	authorParagraph.textContent = data.author;
-	quoteID.textContent = data.number;
-	quoteCount.textContent = data.quotesCount;
+const appendInformation = async (data) => {
+	try {
+		quoteParagraph.textContent = data.quote;
+		authorParagraph.textContent = data.author;
+		quoteID.textContent = data.number;
+		quoteCount.textContent = data.quotesCount;
+		return data;
+	} catch (e) {
+		console.log(e);
+	}
 };
 
-getApi().then(getRandomQuote).then(appendInformation);
+const getNewQuoteFn = () => {
+	getApi().then(getRandomQuote).then(appendInformation);
+};
+
+getNewQuoteFn();
+newQuoteBtn.addEventListener('click', getNewQuoteFn);
+
+//For shortcut keys
+window.addEventListener('keydown', (e) => {
+	switch (e.code) {
+		case 'Enter':
+			getNewQuoteFn();
+			break;
+		case 'KeyC':
+			console.log('Key C');
+			break;
+		default:
+			return "There's no shortcut for that key";
+	}
+});
+
+// navigator.clipboard.writeText(data.quote);
